01698180b5ea880ebc0e61a2674958afb85a73db
Ldoy <leedoy7989@gmail.com>
2021-06-15 13:51:54 +0900

    UIlabel 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,14 +7,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	let juicemaker = JuiceMaker()
+	
+	var strawberryStore: Int { return juicemaker.fruitStores.filter{ $0.name == .strawberry}.first?.stock ?? 0
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-		
-		
+		self.strawberry.text = "\(strawberryStore)"
 		}
-
-
+	
+	
+	
+	@IBAction func strawberryJuiceOrder(_ sender: UIButton) {
+		
+	
+	}
+	
+	@IBOutlet weak var strawberry: UILabel!
+	
+	@IBOutlet weak var banana: UILabel!
+	
+	
+	
 }
 
